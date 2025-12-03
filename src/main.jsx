f789e3ebaaf176e3596012560027c06f61a2e9b6
arup <arupjyotilahkar10@gmail.com>
2025-12-04 00:32:05 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Make sure extension is .jsx
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App.jsx'
+import './index.css';
 
-const rootElement = document.getElementById('root')
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
+// Force dark mode globally (black background, white text)
+document.documentElement.classList.add('dark');
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
